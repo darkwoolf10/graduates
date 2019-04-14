959ed9e4801fa26a81ba86e8cfb7b02d16952067
darkwoolf10 <woolftreuser@yandex.ru>
2019-04-14 16:40:15 +0300

    init project and models

---
+++ crud/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Ball)
+admin.site.register(Curator)
+admin.site.register(Diploma)
+admin.site.register(Group)
+admin.site.register(Work)
+admin.site.register(Student)
+admin.site.register(Faculty)
+
---
+++ crud/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+# Create your models here.
+
+
+class Curator(models.Model):
+    name = models.CharField(max_length=32)
+    surname = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name + " " + self.surname
+
+
+class Diploma(models.Model):
+    title = models.CharField(max_length=128)
+    curator = models.ForeignKey(Curator, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Ball(models.Model):
+    subject = models.CharField(max_length=64)
+    assessment = models.IntegerField(
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(1)
+        ]
+    )
+
+    def __str__(self):
+        return self.subject
+
+
+class Work(models.Model):
+    title = models.CharField(max_length=64)
+    city = models.CharField(max_length=64, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=32)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    curator = models.ForeignKey(Curator, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=32)
+    surname = models.CharField(max_length=32)
+    patronymic = models.CharField(max_length=32, null=True)
+    photo = models.FilePathField(path='/front/assets/image', null=True)
+    diploma = models.ForeignKey(Diploma, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    ball = models.ForeignKey(Ball, on_delete=models.CASCADE)
+    work = models.ForeignKey(Work, on_delete=models.CASCADE)
+    curator = models.ForeignKey(Curator, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name + " " + self.surname
