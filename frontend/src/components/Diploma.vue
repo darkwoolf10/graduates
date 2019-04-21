cf8881f442220248eb274275723f12dee35775d6
darkwoolf <woolftreuser@gmail.com>
2019-04-21 13:25:25 +0300

    make diplomas, start doing students

---
+++ frontend/src/components/Diploma.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Diploma"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/StudentCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-card>
+      <v-img
+        src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+        aspect-ratio="1"
+      ></v-img>
+
+      <v-card-title primary-title>
+        <div>
+          <h3 class="headline mb-0">{{student.name}} {{student.surname}}</h3>
+          <div> </div>
+        </div>
+      </v-card-title>
+
+      <v-card-actions>
+        <v-btn flat color="orange">Показать</v-btn>
+      </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "StudentsCard",
+        props: {
+          student: null
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
