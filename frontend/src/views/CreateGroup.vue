bd27f1348ec897cbcb1f70c42d108fbe6ce39ba7
darkwoolf <woolftreuser@gmail.com>
2019-05-18 09:53:36 +0300

    edit create group component

--- frontend/src/views/CreateGroup.vue
+++ frontend/src/views/CreateGroup.vue
@@ -6,18 +6,22 @@
       label="Group"
     ></v-text-field>
     <v-select
-      v-model="selectedFacylty"
-      :items="faculty"
+      v-model="facylty"
+      :items="facultys"
       label="Выбрать факультет"
+      item-text="name"
+      item-value="id"
     ></v-select>
     <v-select
-      v-model="selectedCurator"
+      v-model="curator"
+      name="curator"
       :items="curators"
       label="Выбрать куратора"
       item-text="name"
+      item-value="id"
     ></v-select>
     <p class="text-xs-right">
-      <v-btn large>Создать</v-btn>
+      <v-btn large @click="createGroup">Создать</v-btn>
     </p>
   </form>
   </v-container>
@@ -28,29 +32,32 @@
         name: "CreateGroup",
         data () {
           return {
-            faculty: null,
+            facultys: null,
             curators: null,
+            curator: null,
             group: null,
-            selectedFacylty: null,
-            selectedCurator: null
+            facylty: null
           }
         },
         async mounted() {
           let vm = this;
           await this.axios.get('http://127.0.0.1:8000/api/faculty/')
             .then((response) => {
-            response.data.forEach(function(element) {
-              vm.faculty = element.name;
-            });
+              vm.facultys = response.data;
           });
           await this.axios.get('http://127.0.0.1:8000/api/curators/')
             .then((response) => {
               vm.curators = response.data;
-              // console.log(response.data);
-              // response.data.forEach(function (element) {
-              //   vm.curators = element.name + element.surname;
-              // })
             })
+        },
+        methods: {
+          createGroup: function () {
+            this.axios.post('http://127.0.0.1:8000/api/groups/', {
+              
+            })
+              .then(response => response.data)
+              .catch(error => console.log(error));
+          }
         }
     }
 </script>
